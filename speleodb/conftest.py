6f4510ca9f934de480fbc0a6696339d3a703740b
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2024-04-28 12:40:39 -0700

    Initial commit

---
+++ speleodb/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from speleodb.users.models import User
+from speleodb.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
