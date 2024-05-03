b9298aa0a66440ff03710f4331806a0f53f56cb2
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-05-03 01:41:28 -0400

    Upload View in Progress

---
+++ speleodb/surveys/api/v1/view_cls.py
@@ -0,0 +1,53 @@
+from rest_framework import exceptions
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from speleodb.surveys.api.v1.utils import SortedResponse
+from speleodb.surveys.api.v1.utils import get_timestamp
+
+
+class CustomAPIView(APIView):
+    def base_request(self, view_fn, request, *args, **kwargs):
+        try:
+            view_fn = getattr(self, view_fn)
+        except AttributeError as e:
+            raise exceptions.MethodNotAllowed(request.method) from e
+
+        payload = {}
+        http_status = status.HTTP_200_OK
+        try:
+            response = view_fn(request, *args, **kwargs)
+
+            if isinstance(response, Response):
+                payload.update(response.data)
+                http_status = response.status_code
+
+            elif isinstance(response, (list, tuple, dict)):
+                payload["data"] = response
+
+            else:
+                payload["data"] = f"Unsupported response type: {type(response)}"
+                http_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+        except Exception as e:  # noqa: BLE001
+            payload["data"] = {}
+            payload["error"] = f"An error occured in the process: {e}"
+            http_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+        payload["url"] = request.build_absolute_uri()
+        payload["timestamp"] = get_timestamp()
+        payload["success"] = http_status in range(200, 300)
+        return SortedResponse(payload, status=http_status)
+
+    def get(self, request, *args, **kwargs):
+        return self.base_request("_get", request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.base_request("_post", request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.base_request("_put", request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.base_request("_delete", request, *args, **kwargs)
--- speleodb/surveys/api/v1/views.py
+++ speleodb/surveys/api/v1/views.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+
 from django.core.exceptions import ValidationError
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import permissions
@@ -10,7 +12,8 @@ from rest_framework.response import Response
 from speleodb.surveys.api.v1.exceptions import NotAuthorizedError
 from speleodb.surveys.api.v1.exceptions import ResourceBusyError
 from speleodb.surveys.api.v1.serializers import ProjectSerializer
-from speleodb.surveys.api.v1.utils import CustomAPIView
+from speleodb.surveys.api.v1.serializers import UploadSerializer
+from speleodb.surveys.api.v1.view_cls import CustomAPIView
 from speleodb.surveys.models import Permission
 from speleodb.surveys.models import Project
 
@@ -19,8 +22,7 @@ class ProjectAcquireApiView(CustomAPIView):
     # add permission to check if user is authenticated
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ProjectSerializer
-    queryset = Project.objects.all()
-    lookup_field = "pk"
+    http_method_names = ["post"]
 
     def _post(self, request, project_id):
         project = Project.objects.get(id=project_id)
@@ -55,8 +57,7 @@ class ProjectReleaseApiView(CustomAPIView):
     # add permission to check if user is authenticated
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ProjectSerializer
-    queryset = Project.objects.all()
-    lookup_field = "pk"
+    http_method_names = ["post"]
 
     def _post(self, request, project_id):
         project = Project.objects.get(id=project_id)
@@ -91,8 +92,7 @@ class ProjectApiView(CustomAPIView):
     # add permission to check if user is authenticated
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ProjectSerializer
-    queryset = Project.objects.all()
-    lookup_field = "pk"
+    http_method_names = ["get"]
 
     def _get(self, request, project_id):
         project = Project.objects.get(id=project_id)
@@ -103,12 +103,35 @@ class ProjectApiView(CustomAPIView):
         return proj_dict
 
 
+class CreateProjectApiView(CustomAPIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ProjectSerializer
+    http_method_names = ["post"]
+
+    def _post(self, request):
+        """
+        Create the Todo with given todo data
+        """
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            proj = serializer.save()
+            Permission.objects.create(
+                project=proj, user=request.user, level=Permission.Level.OWNER
+            )
+
+            return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
+
+        return Response(
+            {"errror": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
 class ProjectListApiView(CustomAPIView):
     # add permission to check if user is authenticated
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ProjectSerializer
-    queryset = Project.objects.all()
-    lookup_field = "pk"
+    http_method_names = ["get"]
 
     def _get(self, request):
         usr_projects = [
@@ -130,19 +153,46 @@ class ProjectListApiView(CustomAPIView):
 
         return results
 
-    # 2. Create
-    @csrf_exempt
-    def post(self, request, *args, **kwargs):
-        """
-        Create the Todo with given todo data
-        """
-        serializer = ProjectSerializer(data=request.data)
-        if serializer.is_valid():
-            proj = serializer.save()
-            Permission.objects.create(
-                project=proj, user=request.user, level=Permission.Level.OWNER
-            )
 
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+class FileUploadView(CustomAPIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UploadSerializer
+    http_method_names = ["put"]
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def _put(self, request, project_id):
+        file_uploaded = request.FILES.get("file_uploaded")
+        content_type = file_uploaded.content_type
+
+        if content_type != "application/octet-stream":
+            data = {
+                "error": (
+                    f"Unknown MIME Type received: `{content_type}`. "
+                    "Expected: `application/octet-stream`."
+                )
+            }
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+
+        f_extension = Path(file_uploaded.name).suffix.lower()
+        if f_extension != ".tml":
+            data = {
+                "error": (
+                    f"Unknown file extension received: `{f_extension}`. "
+                    "Expected: `.tml`."
+                )
+            }
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+        # file_uploaded =>
+        # {
+        #     '_name': 'test_simple.tml',
+        #     'charset': None,
+        #     'content_type': 'application/octet-stream',
+        #     'content_type_extra': {},
+        #     'field_name': 'file_uploaded',
+        #     'file': <_io.BytesIO object at 0x71e8c6b982c0>,
+        #     'size': 92424
+        # }
+        return {
+            "data": f"PUT API and you have uploaded a {content_type} file",
+            "project_id": project_id,
+        }
