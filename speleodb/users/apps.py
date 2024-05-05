685300c1cb9a80fe36a8dcd7b3ff43540b2e1a4f
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-05-05 19:13:15 -0400

    Adding Encrypted Options

--- speleodb/users/apps.py
+++ speleodb/users/apps.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class UsersConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "speleodb.users"
     verbose_name = _("Users")
 
