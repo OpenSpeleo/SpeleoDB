4bfccb336ecf43382cc13aa5af9c5328bf0780da
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-05-05 17:01:27 -0400

    Env File reading Setup

--- config/settings/local.py
+++ config/settings/local.py
@@ -9,10 +9,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env(
-    "DJANGO_SECRET_KEY",
-    default="OWN0g4QdS8pQWbQjfeKiJkr0M3Ue9bKGayDWavq0j41op9tIgheqHcIQxaQqurdb",
-)
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
 
@@ -33,7 +30,7 @@ EMAIL_BACKEND = env(
     "DJANGO_EMAIL_BACKEND",
     default="django.core.mail.backends.filebased.EmailBackend",
 )
-EMAIL_FILE_PATH = "./.tmp/app-messages"
+EMAIL_FILE_PATH = "./dev_tmp/emails"
 
 # WhiteNoise
 # ------------------------------------------------------------------------------
