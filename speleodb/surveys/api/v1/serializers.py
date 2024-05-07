60f2b26e30fb04df640bc1b35058ea17ca7ed8a2
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-05-07 00:46:21 -0400

    Update Project Serializer to include user permission

--- speleodb/surveys/api/v1/serializers.py
+++ speleodb/surveys/api/v1/serializers.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
-from rest_framework.serializers import FileField
-from rest_framework.serializers import Serializer
 
 from speleodb.surveys.models import Project
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    permission = serializers.SerializerMethodField()
+
     class Meta:
         model = Project
         fields = "__all__"
 
+    def get_permission(self, obj):
+        user = self.context.get("user")
+        try:
+            return obj.get_permission(user=user).level_name
+        except ObjectDoesNotExist:
+            return None
+
 
 # Serializers define the API representation.
-class UploadSerializer(Serializer):
-    file_uploaded = FileField()
+class UploadSerializer(serializers.Serializer):
+    file_uploaded = serializers.FileField()
 
     class Meta:
         fields = ["file_uploaded"]
