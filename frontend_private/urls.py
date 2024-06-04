da9dcd0a7cf69dec4e9010e4a25afe19c1d29804
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-06-04 01:37:32 -0400

    User Panel in progress

--- frontend_private/urls.py
+++ frontend_private/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
 from frontend_private.views import DashboardView
+from frontend_private.views import FeedbackView
+from frontend_private.views import NotificationsView
+from frontend_private.views import ProjectDetailView
 from frontend_private.views import ProjectView
-from frontend_private.views import AccountView
 
 app_name = "private"
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
+    path("feedback/", FeedbackView.as_view(), name="feedback"),
+    path("notifications/", NotificationsView.as_view(), name="notifications"),
     path("projects/", ProjectView.as_view(), name="projects"),
-    path("account/", AccountView.as_view(), name="account"),
+    path("project/<uuid:id>/", ProjectDetailView.as_view(), name="project_details"),
 ]
