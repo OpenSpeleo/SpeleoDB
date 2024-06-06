d5ca2628481b176f520de0adf43266971b8d3248
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-06-06 03:07:46 -0400

    [Project] Mutex, History, Danger Zone and Permission View implemented

--- frontend_private/urls.py
+++ frontend_private/urls.py
@@ -3,16 +3,19 @@ from django.urls import path
 from frontend_private.views import DashboardView
 from frontend_private.views import FeedbackView
 from frontend_private.views import NotificationsView
+from frontend_private.views import ProjectCommitsView
+from frontend_private.views import ProjectDangerZoneView
 from frontend_private.views import ProjectDetailsView
+from frontend_private.views import ProjectListingView
+from frontend_private.views import ProjectMutexesView
 from frontend_private.views import ProjectPermissionsView
-from frontend_private.views import ProjectView
 
 app_name = "private"
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
     path("feedback/", FeedbackView.as_view(), name="feedback"),
     path("notifications/", NotificationsView.as_view(), name="notifications"),
-    path("projects/", ProjectView.as_view(), name="projects"),
+    path("projects/", ProjectListingView.as_view(), name="projects"),
     path(
         "project/<uuid:project_id>/",
         ProjectDetailsView.as_view(),
@@ -22,5 +25,20 @@ urlpatterns = [
         "project/<uuid:project_id>/permissions/",
         ProjectPermissionsView.as_view(),
         name="project_permissions",
+    ),
+    path(
+        "project/<uuid:project_id>/mutexes/",
+        ProjectMutexesView.as_view(),
+        name="project_mutexes",
+    ),
+    path(
+        "project/<uuid:project_id>/revisions/",
+        ProjectCommitsView.as_view(),
+        name="project_revisions",
+    ),
+    path(
+        "project/<uuid:project_id>/danger_zone/",
+        ProjectDangerZoneView.as_view(),
+        name="project_danger_zone",
     ),
 ]
--- frontend_private/views.py
+++ frontend_private/views.py
@@ -1,5 +1,9 @@
+import datetime
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import TemplateView
@@ -25,23 +29,77 @@ class NotificationsView(_AuthenticatedTemplateView):
 
 
 # ============ Project Pages ============ #
-class ProjectView(_AuthenticatedTemplateView):
+class ProjectListingView(_AuthenticatedTemplateView):
     template_name = "pages/projects.html"
 
 
-class ProjectDetailsView(LoginRequiredMixin, View):
+class _BaseProjectView(LoginRequiredMixin, View):
+    def get(self, request, project_id: str):
+        project = Project.objects.get(id=project_id)
+        return {
+            "project": project,
+            "is_project_owner": project.is_owner(request.user),
+            "has_write_access": project.has_write_access(request.user),
+        }
+
+
+class ProjectDangerZoneView(_BaseProjectView):
+    template_name = "pages/project/danger_zone.html"
+
+    def get(self, request, project_id: str):
+        data = super().get(request, project_id=project_id)
+
+        if not data["is_project_owner"]:
+            return redirect(
+                reverse("private:project_details", kwargs={"project_id": project_id})
+            )
+
+        return render(request, ProjectDangerZoneView.template_name, data)
+
+
+class ProjectDetailsView(_BaseProjectView):
     template_name = "pages/project/details.html"
 
     def get(self, request, project_id: str):
-        project = Project.objects.get(id=project_id)
-        return render(request, ProjectDetailsView.template_name, {"project": project})
+        data = super().get(request, project_id=project_id)
+        return render(request, ProjectDetailsView.template_name, data)
 
 
-class ProjectPermissionsView(LoginRequiredMixin, View):
+class ProjectPermissionsView(_BaseProjectView):
     template_name = "pages/project/permissions.html"
 
     def get(self, request, project_id: str):
-        project = Project.objects.get(id=project_id)
-        return render(
-            request, ProjectPermissionsView.template_name, {"project": project}
+        data = super().get(request, project_id=project_id)
+        data["permissions"] = data["project"].rel_permissions.all()
+        return render(request, ProjectPermissionsView.template_name, data)
+
+
+class ProjectMutexesView(_BaseProjectView):
+    template_name = "pages/project/mutex_history.html"
+
+    def get(self, request, project_id: str):
+        data = super().get(request, project_id=project_id)
+        data["mutexes"] = data["project"].rel_mutexes.all().order_by("-creation_dt")
+        return render(request, ProjectMutexesView.template_name, data)
+
+
+class ProjectCommitsView(_BaseProjectView):
+    template_name = "pages/project/commit_history.html"
+
+    def get(self, request, project_id: str):
+        data = super().get(request, project_id=project_id)
+
+        commits = sorted(
+            data["project"].commit_history,
+            key=lambda record: record["authored_date"],
+            reverse=True,
         )
+        for commit in commits:
+            for key in ["authored_date", "committed_date", "created_at"]:
+                if isinstance(commit[key], str):
+                    commit[key] = datetime.datetime.strptime(  # noqa: DTZ007
+                        commit[key].split(".")[0], "%Y-%m-%dT%H:%M:%S"
+                    )
+
+        data["commits"] = commits
+        return render(request, ProjectCommitsView.template_name, data)
