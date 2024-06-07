7eca3ef386d193b20edb354e2dd0bf0c347a7391
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-06-07 00:03:46 -0400

    Fix Attempt for error pages

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,7 @@ from django.views import defaults as default_views
 
 urlpatterns = [
     path("", include("frontend_public.urls")),
+    path("", include("frontend_errors.urls")),
     path("", include("speleodb.urls")),
     path("private/", include("frontend_private.urls", namespace="private")),
     # # Django Admin, use {% url 'admin:index' %}
@@ -21,27 +22,6 @@ if settings.DEBUG:
     # Static file serving when using Gunicorn + Uvicorn for local web socket development
     urlpatterns += staticfiles_urlpatterns()
 
-if settings.DEBUG:
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
-    urlpatterns += [
-        path(
-            "400/",
-            default_views.bad_request,
-            kwargs={"exception": Exception("Bad Request!")},
-        ),
-        path(
-            "403/",
-            default_views.permission_denied,
-            kwargs={"exception": Exception("Permission Denied")},
-        ),
-        path(
-            "404/",
-            default_views.page_not_found,
-            kwargs={"exception": Exception("Page not Found")},
-        ),
-        path("500/", default_views.server_error),
-    ]
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
--- frontend_errors/urls.py
+++ frontend_errors/urls.py
@@ -1,9 +1,50 @@
+from django.conf import settings
+from django.urls import path
+from django.views import defaults as default_views
 from django.views.defaults import ERROR_400_TEMPLATE_NAME
 from django.views.defaults import ERROR_403_TEMPLATE_NAME
 from django.views.defaults import ERROR_404_TEMPLATE_NAME
 from django.views.defaults import ERROR_500_TEMPLATE_NAME
 
+from frontend_private.views import DashboardView
+from frontend_private.views import FeedbackView
+from frontend_private.views import NotificationsView
+from frontend_private.views import ProjectCommitsView
+from frontend_private.views import ProjectDangerZoneView
+from frontend_private.views import ProjectDetailsView
+from frontend_private.views import ProjectListingView
+from frontend_private.views import ProjectMutexesView
+from frontend_private.views import ProjectPermissionsView
+
 ERROR_400_TEMPLATE_NAME = "400.html"  # noqa: F811
 ERROR_403_TEMPLATE_NAME = "403.html"  # noqa: F811
 ERROR_404_TEMPLATE_NAME = "404.html"  # noqa: F811
 ERROR_500_TEMPLATE_NAME = "500.html"  # noqa: F811
+
+app_name = "errors"
+
+if settings.DEBUG:
+    # This allows the error pages to be debugged during development, just visit
+    # these url in browser to see how these error pages look like.
+
+    urlpatterns = [
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
+    ]
+
+else:
+    urlpatterns = []
