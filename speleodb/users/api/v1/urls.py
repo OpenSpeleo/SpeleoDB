736ade612d1423957372e52113651663f181d3ed
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-06-07 11:23:55 -0400

    BugFix

--- speleodb/users/api/v1/urls.py
+++ speleodb/users/api/v1/urls.py
@@ -3,12 +3,12 @@
 
 from django.urls import path
 
-from speleodb.users.api.v1.views import UserPreference
 from speleodb.users.api.v1.views import ObtainAuthToken
+from speleodb.users.api.v1.views import UserPreference
 
 app_name = "user_api"
 
 urlpatterns = [
     path("user/preferences/", UserPreference.as_view(), name="set_user_preferences"),
-    path("user/auth-token/", ObtainAuthToken.as_view()),
+    path("user/auth-token/", ObtainAuthToken.as_view(), name="get_auth_token"),
 ]
