fa4296f718e352047cd957e9072fab65c17c0d6c
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-06-07 00:22:29 -0400

    Renamed "notifications" into "preferences"

--- frontend_private/urls.py
+++ frontend_private/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from frontend_private.views import DashboardView
 from frontend_private.views import FeedbackView
-from frontend_private.views import NotificationsView
+from frontend_private.views import PreferencesView
 from frontend_private.views import ProjectCommitsView
 from frontend_private.views import ProjectDangerZoneView
 from frontend_private.views import ProjectDetailsView
@@ -14,7 +14,7 @@ app_name = "private"
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
     path("feedback/", FeedbackView.as_view(), name="feedback"),
-    path("notifications/", NotificationsView.as_view(), name="notifications"),
+    path("preferences/", PreferencesView.as_view(), name="preferences"),
     path("projects/", ProjectListingView.as_view(), name="projects"),
     path(
         "project/<uuid:project_id>/",
