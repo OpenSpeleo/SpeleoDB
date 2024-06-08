66a7a8e4d88f7ba975cb2a3a07fd58984b75bd63
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-06-08 19:39:44 -0400

    Add API to change User Password

--- speleodb/users/api/v1/urls.py
+++ speleodb/users/api/v1/urls.py
@@ -4,11 +4,15 @@
 from django.urls import path
 
 from speleodb.users.api.v1.views import ObtainAuthToken
+from speleodb.users.api.v1.views import UserPasswordChangeView
 from speleodb.users.api.v1.views import UserPreference
 
 app_name = "user_api"
 
 urlpatterns = [
+    path(
+        "user/password/", UserPasswordChangeView.as_view(), name="change_user_password"
+    ),
     path("user/preferences/", UserPreference.as_view(), name="set_user_preferences"),
     path("user/auth-token/", ObtainAuthToken.as_view(), name="get_auth_token"),
 ]
