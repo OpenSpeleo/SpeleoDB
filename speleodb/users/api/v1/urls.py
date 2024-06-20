f6941688f7a897b1e041e3f983771bcbb43c88e6
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-06-20 00:15:38 -0400

    Renamed `update_user_info` into `user_info`

--- speleodb/users/api/v1/urls.py
+++ speleodb/users/api/v1/urls.py
@@ -11,7 +11,7 @@ app_name = "user_api"
 
 urlpatterns = [
     path("user/auth-token/", UserAuthTokenView.as_view(), name="auth_token"),
-    path("user/info/", UserInfo.as_view(), name="update_user_info"),
+    path("user/info/", UserInfo.as_view(), name="user_info"),
     path(
         "user/password/", UserPasswordChangeView.as_view(), name="update_user_password"
     ),
