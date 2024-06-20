6023339da67c8231c495fe9a6c4fc4d0be4bbb36
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-06-19 20:48:18 -0400

    Fixed Password Change Page

--- speleodb/users/api/v1/views.py
+++ speleodb/users/api/v1/views.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from allauth.account import signals
-from allauth.account.adapter import get_adapter
-from allauth.account.internal.flows.password_change import logout_on_password_change
-from django.conf import settings
-from django.contrib.auth.password_validation import validate_password
-from django.core.exceptions import ValidationError
 from rest_framework import permissions
 from rest_framework import status
 from rest_framework.authtoken.models import Token
@@ -16,6 +10,7 @@ from rest_framework.settings import api_settings
 from rest_framework.throttling import UserRateThrottle
 
 from speleodb.users.api.v1.serializers import AuthTokenSerializer
+from speleodb.users.api.v1.serializers import PasswordChangeSerializer
 from speleodb.users.api.v1.serializers import UserSerializer
 from speleodb.utils.response import ErrorResponse
 from speleodb.utils.response import NoWrapResponse
@@ -106,51 +101,21 @@ class PasswordChangeThrottle(UserRateThrottle):
 
 
 class UserPasswordChangeView(GenericAPIView):
-    serializer_class = UserSerializer
+    serializer_class = PasswordChangeSerializer
     throttle_classes = [PasswordChangeThrottle]
     permission_classes = [permissions.IsAuthenticated]
     http_method_names = ["put"]
 
     def put(self, request, *args, **kwargs):
-        try:
-            oldpassword = request.data["oldpassword"]
-            password1 = request.data["password1"]
-            password2 = request.data["password2"]
-        except KeyError as e:
-            return ErrorResponse(
-                {"error": f"Attribute: {e} is missing"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        try:
-            if password1 != password2:
-                raise ValidationError("Password mismatch: `password1` != `password2`")  # noqa: TRY301
-
-            if not request.user.check_password(oldpassword):
-                raise ValidationError("Current password is not valid: `oldpassword`")  # noqa: TRY301
-
-            if oldpassword == password1:
-                raise ValidationError("The new and old password are identical")  # noqa: TRY301
-
-            if not settings.DEBUG:
-                validate_password(password1, user=request.user)
-
-        except ValidationError as e:
-            return ErrorResponse(
-                {"errors": e.messages},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        get_adapter(request).set_password(request.user, password1)
-        request.user.save()
-        signals.password_changed.send(
-            sender=request.user.__class__,
-            request=request,
-            user=request.user,
+        serializer = self.get_serializer(
+            data=request.data, partial=True, context={"request": request}
         )
+        if serializer.is_valid():
+            serializer.save()
+            return NoWrapResponse(
+                {"message": "Password changed successfully"}, status=status.HTTP_200_OK
+            )
 
-        logout_on_password_change(request, request.user)
-
-        return NoWrapResponse(
-            {"message": "Password changed successfully"}, status=status.HTTP_200_OK
+        return ErrorResponse(
+            {"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
         )
