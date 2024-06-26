cf8245ffe8b217c355997fedc1a359f1121ef7e6
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-06-26 15:39:36 -0400

    Add Compass Support

--- speleodb/processors/_impl/ariane.py
+++ speleodb/processors/_impl/ariane.py
@@ -17,3 +17,11 @@ class TMLUFileProcessor(BaseFileProcessor):
     TARGET_SAVE_FILENAME = "project.tmlu"
     TARGET_DOWNLOAD_FILENAME = "project_{timestamp}.tmlu"
     ASSOC_FILEFORMAT = Format.FileFormat.ARIANE_TMLU
+
+
+class AGRFileProcessor(BaseFileProcessor):
+    ALLOWED_EXTENSIONS = [".agr"]
+    ALLOWED_MIMETYPES = ["text/plain"]
+    TARGET_SAVE_FILENAME = "project.agr"
+    TARGET_DOWNLOAD_FILENAME = "project_{timestamp}.agr"
+    ASSOC_FILEFORMAT = Format.FileFormat.ARIANE_AGR
