7e959ae84e0a407ff019d145b24765c466936f06
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-07-01 10:20:07 -0400

    git timestamp to datetime fixed

--- frontend_private/templatetags/filter_utils.py
+++ frontend_private/templatetags/filter_utils.py
@@ -11,25 +11,29 @@ from django.utils import timezone
 register = template.Library()
 
 
+def datetime_of_struct_time(st: time.struct_time) -> datetime.datetime:
+    "Convert a struct_time to datetime maintaining timezone information when present"
+    tz = None
+    if st.tm_gmtoff is not None:
+        tz = datetime.timezone(datetime.timedelta(seconds=st.tm_gmtoff))
+    # datetime doesn't like leap seconds so just truncate to 59 seconds
+    if st.tm_sec in {60, 61}:
+        dt = datetime.datetime(*st[:5], 59, tzinfo=tz)
+    dt = datetime.datetime(*st[:6], tzinfo=tz)
+
+    return dt.astimezone(pytz.timezone(settings.TIME_ZONE))
+
+
 @register.filter(name="time_struct_since")
 def time_struct_since(time_struct: time.struct_time):
-    dt = datetime.datetime.fromtimestamp(
-        time.mktime(time_struct), tz=pytz.timezone(settings.TIME_ZONE)
-    )
+    dt = datetime_of_struct_time(time_struct)
     dt_now = timezone.now()
     return timeago.format(dt, dt_now)
 
 
 @register.filter(name="time_struct_format")
 def time_struct_format(time_struct: time.struct_time):
-    dt = datetime.datetime.fromtimestamp(time.mktime(time_struct))  # noqa: DTZ006
-
-    source_tz = pytz.timezone("Etc/GMT-1")
-    dt = source_tz.localize(dt)
-
-    timezone = pytz.timezone("US/Eastern")
-    dt = dt.astimezone(timezone)
-
+    dt = datetime_of_struct_time(time_struct)
     return dt.strftime("%Y/%m/%d %H:%M")
 
 
