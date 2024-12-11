9806484fd94d5233eb0f52cdd70a608ce6788a14
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2024-12-10 22:40:01 -0500

    [Unittests] Refactoring & File Upload testing

--- speleodb/api/v1/tests/test_file_upload.py
+++ speleodb/api/v1/tests/test_file_upload.py
@@ -1,7 +1,7 @@
 import pathlib
-import random
 import re
 
+import pytest
 from django.test import TestCase
 from django.urls import reverse
 from parameterized import parameterized
@@ -9,18 +9,12 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from speleodb.api.v1.serializers import ProjectSerializer
-from speleodb.api.v1.tests.factories import ProjectFactory
-from speleodb.api.v1.tests.factories import SurveyTeamFactory
-from speleodb.api.v1.tests.factories import TeamPermissionFactory
-from speleodb.api.v1.tests.factories import TokenFactory
-from speleodb.api.v1.tests.factories import UserFactory
-from speleodb.api.v1.tests.factories import UserPermissionFactory
+from speleodb.api.v1.tests.base_project_testcase import AnyPermissionLevel
+from speleodb.api.v1.tests.base_project_testcase import BaseProjectTestCase
 from speleodb.surveys.models import Format
 from speleodb.surveys.models import TeamPermission
 from speleodb.surveys.models import UserPermission
-from speleodb.users.models import SurveyTeamMembership
-
-AnyPermissionLevel = UserPermission.Level | TeamPermission.Level
+from speleodb.utils.test_utils import named_product
 
 BASE_DIR = pathlib.Path(__file__).parent / "artifacts"
 TEST_FILES = [
@@ -40,51 +34,26 @@ def is_subset(subset_dict, super_dict):
     return all(item in super_dict.items() for item in subset_dict.items())
 
 
-class FileViewTests(TestCase):
-    """Token authentication"""
-
-    header_prefix = "Token "
-
-    def setUp(self):
-        self.client = APIClient(enforce_csrf_checks=False)
-
-        self.user = UserFactory()
-        self.token = TokenFactory(user=self.user)
-        self.project = ProjectFactory()
-
-    def set_test_permission(self, level: AnyPermissionLevel):
-        if isinstance(level, UserPermission.Level):
-            _ = UserPermissionFactory(
-                target=self.user, level=level, project=self.project
-            )
-
-        elif isinstance(level, TeamPermission.Level):
-            # Create a team for the user - assign the user to the team
-            team = SurveyTeamFactory()
-            _ = SurveyTeamMembership.objects.create(
-                user=self.user,
-                team=team,
-                role=random.choice(SurveyTeamMembership.Role.values),
-            )
-
-            # Give the newly created permission to the project
-            _ = TeamPermissionFactory(
-                target=team,
-                level=level,
-                project=self.project,
-            )
-
-        else:
-            raise TypeError(f"Received unexpected level type: `{type(level)}`")
-
-    @parameterized.expand(TEST_FILES)
-    def test_upload_valid_file(self, testfile: pathlib.Path):
+class FileViewTests(BaseProjectTestCase):
+    @parameterized.expand(
+        named_product(
+            testfile=TEST_FILES,
+            level=[
+                UserPermission.Level.ADMIN,
+                UserPermission.Level.READ_AND_WRITE,
+                TeamPermission.Level.READ_AND_WRITE,
+            ],
+        )
+    )
+    def test_upload_valid_file(
+        self, testfile: pathlib.Path, uploader_access_level: AnyPermissionLevel
+    ):
         """
         Test file uploads with valid formats.
         """
         assert testfile.exists()
 
-        self.set_test_permission(level=UserPermission.Level.ADMIN)
+        self.set_test_permission(level=uploader_access_level)
 
         self.project.acquire_mutex(self.user)
 
@@ -137,14 +106,77 @@ class FileViewTests(TestCase):
             "response_data": response_data["project"],
         }
 
-    @parameterized.expand(TEST_FILES)
-    def test_upload_file_error_without_mutex(self, testfile: pathlib.Path):
+    @parameterized.expand(
+        named_product(
+            testfile=TEST_FILES,
+            level=[
+                UserPermission.Level.ADMIN,
+                UserPermission.Level.READ_AND_WRITE,
+                TeamPermission.Level.READ_AND_WRITE,
+            ],
+        )
+    )
+    def test_upload_file_error_without_mutex(
+        self, testfile: pathlib.Path, uploader_access_level: AnyPermissionLevel
+    ):
         """
         Test file uploads with valid formats.
         """
         assert testfile.exists()
 
-        self.set_test_permission(level=UserPermission.Level.ADMIN)
+        self.set_test_permission(level=uploader_access_level)
+
+        fileformat = None
+        match testfile.suffix.lstrip(".").upper():
+            case "TML":
+                fileformat = Format.FileFormat.ARIANE_TML
+            case "TMLU":
+                fileformat = Format.FileFormat.ARIANE_TMLU
+            case _:
+                raise ValueError(
+                    f"Unknown value: `{testfile.suffix.lstrip('.').upper()}`"
+                )
+
+        commit_message = "Valid commit message"
+
+        with testfile.open(mode="rb") as file_data:
+            auth = self.header_prefix + self.token.key
+            response = self.client.put(
+                reverse(
+                    "api:v1:upload_project",
+                    kwargs={
+                        "id": self.project.id,
+                        "fileformat": fileformat.label.lower(),
+                    },
+                ),
+                {"artifact": file_data, "message": commit_message},
+                format="multipart",
+                headers={"authorization": auth},
+            )
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN, response.data
+
+    @parameterized.expand(
+        named_product(
+            testfile=TEST_FILES,
+            level=[
+                UserPermission.Level.READ_ONLY,
+                TeamPermission.Level.READ_ONLY,
+            ],
+        )
+    )
+    def test_upload_error_in_readonly(
+        self, testfile: pathlib.Path, uploader_access_level: AnyPermissionLevel
+    ):
+        """
+        Test file uploads with valid formats.
+        """
+        assert testfile.exists()
+
+        self.set_test_permission(level=uploader_access_level)
+
+        with pytest.raises(PermissionError):
+            self.project.acquire_mutex(self.user)
 
         fileformat = None
         match testfile.suffix.lstrip(".").upper():
