47a91bd4458ca5aeeaf56317035ec686dd3e1d61
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-01-03 12:28:55 -0500

    [BugFix] Zip Download

--- speleodb/processors/_impl/dump.py
+++ speleodb/processors/_impl/dump.py
@@ -1,10 +1,8 @@
 import zipfile
 from pathlib import Path
 
-from git.exc import GitCommandError
-
+from speleodb.git_engine.core import GitCommit
 from speleodb.git_engine.core import GitFile
-from speleodb.git_engine.exceptions import GitBaseError
 from speleodb.processors.base import BaseFileProcessor
 from speleodb.surveys.models import Format
 
@@ -13,21 +11,13 @@ class DumpProcessor(BaseFileProcessor):
     TARGET_DOWNLOAD_FILENAME = "{project_name}__{timestamp}.zip"
     ASSOC_FILEFORMAT = Format.FileFormat.DUMP
 
-    def _generate_or_copy_file_for_download(self, target_f: Path) -> None:
+    def _generate_or_copy_file_for_download(
+        self, commit: GitCommit, target_f: Path
+    ) -> None:
+        if not isinstance(commit, GitCommit):
+            raise TypeError(f"Unexpected type for `commit`: {type(commit)}.")
+
         with zipfile.ZipFile(target_f, "w", compression=zipfile.ZIP_DEFLATED) as zipf:
-            try:
-                try:
-                    commit = self.project.git_repo.commit(self.hexsha)
-                except ValueError:  # In case the commit doesn't exist - pull and retry
-                    self.project.git_repo.pull()
-                    commit = self.project.git_repo.commit(self.hexsha)
-
-                if commit is None:
-                    raise ValueError  # noqa: TRY301
-
-            except (ValueError, GitBaseError, GitCommandError) as e:
-                raise RuntimeError(f"Impossible to find commit: `{self.hexsha}`") from e
-
             commit_files = [
                 item for item in commit.tree.traverse() if isinstance(item, GitFile)
             ]
