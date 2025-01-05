530a212d7a0d0f953e1addbf6afdb030f7181832
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-01-05 00:54:22 -0500

    [PreLoading] Git Projects can now be Pre-loaded

---
+++ speleodb/common/management/commands/preload_projects.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+from django.core.management.base import BaseCommand
+
+from speleodb.surveys.models import Project
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = (
+        "Download all the git projects to the local directory. Mostly useful for "
+        "production servers to reduce user waiting time."
+    )
+
+    def handle(self, *args, **kwargs):
+        for project in Project.objects.all():
+            logger.info(f"Processing Project: {project.id}")
+            try:
+                _ = project.git_repo  # load the git project
+            except Exception:
+                logger.exception(f"An error occured with project: {project.id}")
