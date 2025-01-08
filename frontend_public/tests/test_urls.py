4bb313424fca9c18918fb52863f4dd92de40ee79
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-01-08 00:27:07 -0500

    [Unittests] Adding Public URL unittests

---
+++ frontend_public/tests/test_urls.py
@@ -0,0 +1,45 @@
+import pytest
+from django.urls import resolve
+from django.urls import reverse
+
+
+@pytest.mark.parametrize(
+    ("name", "path", "kwargs"),
+    [
+        # General routes
+        ("home", "", None),
+        ("about", "about/", None),
+        ("roadmap", "roadmap/", None),
+        ("changelog", "changelog/", None),
+        ("terms_and_conditions", "terms_and_conditions/", None),
+        ("privacy_policy", "privacy_policy/", None),
+        # Webviews
+        ("webview_ariane", "webview/ariane/", None),
+        # User Auth Management
+        ("account_login", "login/", None),
+        ("account_logout", "logout/", None),
+        ("account_signup", "signup/", None),
+        (
+            "account_confirm_email",
+            "account/confirm-email/{key}/",
+            {"key": "abc123-def456:ghi789"},
+        ),
+        ("account_reset_password", "account/password/reset/", None),
+        (
+            "account_reset_password_from_key",
+            "account/password/reset/{uidb36}-{key}/",
+            {"uidb36": "test@speleodb.org", "key": "abc123-def456"},
+        ),
+    ],
+)
+def test_routes(name: str, path: str, kwargs: dict | None):
+    path = f"/{path}" if kwargs is None else f"/{path.format(**kwargs)}"
+
+    # Test reverse URL generation
+    if kwargs:
+        assert reverse(name, kwargs=kwargs) == path
+    else:
+        assert reverse(name) == path
+
+    # Test resolve to view name
+    assert resolve(path).view_name == name
