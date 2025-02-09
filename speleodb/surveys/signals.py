a607d193ab3e433fd7d85463e6201185c2d01159
DEKHTIARJonathan <jonathan@dekhtiar.com>
2025-02-09 02:16:01 -0500

    WIP Signals

---
+++ speleodb/surveys/signals.py
@@ -0,0 +1,9 @@
+from django.dispatch import Signal
+from django.dispatch import receiver
+
+git_push_done = Signal()
+
+
+@receiver(git_push_done)
+def notify_admin(sender, **kwargs):
+    print(f"Git Push Executed! {sender=} | Task details: {kwargs=}")  # noqa: T201
